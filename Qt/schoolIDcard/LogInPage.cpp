73393414d23eaf3619d8c99ee0880813533a7ace
Recoleta <13276961538@163.com>
2026-09-03 16:33:46 +0800

    Initial commit: school ID card Qt project

---
+++ Qt/schoolIDcard/LogInPage.cpp
@@ -0,0 +1,14 @@
+#include "LogInPage.h"
+#include "ui_LogInPage.h"
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+# Summer_School_cpp_Project
+
+C++ 大作业项目。
+
+## 简介
+
+（待补充项目说明）
+
+## 构建
+
+（待补充构建方法，如 CMake / Makefile / Visual Studio 等）
+
+## 下载
+
+最新版本请前往 [Releases](../../releases) 页面下载。
